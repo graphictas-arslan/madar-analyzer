9367f009c2b3bd86ede4612018d7045b577b3cee
graphictas-arslan <graphictas@gmail.com>
2026-06-20 11:24:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI, Request
+import sqlite3
+import json
 
 app = FastAPI()
+
+# دیتابیس ساده
+conn = sqlite3.connect("madar.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    raw TEXT
+)
+""")
+conn.commit()
 
 @app.get("/")
 def home():
@@ -9,5 +23,14 @@ def home():
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
-    print("NEW UPDATE:", data)
+
+    # ذخیره پیام خام
+    cursor.execute(
+        "INSERT INTO messages (raw) VALUES (?)",
+        (json.dumps(data),)
+    )
+    conn.commit()
+
+    print("SAVED MESSAGE")
+
     return {"ok": True}
