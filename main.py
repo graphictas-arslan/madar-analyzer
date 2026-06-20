ca6eae855b150c895fc375443f1885b91e2a1e26
graphictas-arslan <graphictas@gmail.com>
2026-06-20 11:01:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "ok", "project": "madar-analyzer"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    print("NEW UPDATE:", data)
+    return {"ok": True}
