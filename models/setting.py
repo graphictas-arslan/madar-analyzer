778ef590e6012c4180509d588a510fa170c28665
graphictas-arslan <graphictas@gmail.com>
2026-06-21 17:28:06 +0330

    Update setting.py

--- models/setting.py
+++ models/setting.py
@@ -1,1 +1,35 @@
+from extensions import db
+from datetime import datetime
 
+
+class Setting(db.Model):
+
+    __tablename__ = "settings"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    key = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    value = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"<Setting {self.key}>"
