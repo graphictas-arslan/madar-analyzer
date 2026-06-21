17d36e4274bd4fc4460c5c90a3a991b4b3d531a7
graphictas-arslan <graphictas@gmail.com>
2026-06-21 17:09:50 +0330

    Update organization_user.py

--- models/organization_user.py
+++ models/organization_user.py
@@ -1,1 +1,29 @@
+from extensions import db
+from datetime import datetime
 
+
+class OrganizationUser(db.Model):
+
+    __tablename__ = "organization_users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    organization_id = db.Column(
+        db.Integer,
+        db.ForeignKey("organizations.id"),
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
