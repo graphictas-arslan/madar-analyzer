d0d6308293f014a0dcf1ceb122fca9d95d7abf22
graphictas-arslan <graphictas@gmail.com>
2026-06-21 18:43:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,22 @@ from extensions import db, migrate
 
 import models
 
-# ساخت برنامه
 app = Flask(__name__)
 
-# تنظیمات
 app.config.from_object(Config)
+
 db.init_app(app)
 migrate.init_app(app, db)
 
-# اتصال دیتابیس
+init_database(app)
 
 
 @app.route("/")
 def home():
-
     return "Bale Analyzer is Running 🚀"
 
 
-
 if __name__ == "__main__":
-
     app.run(
         host="0.0.0.0",
         port=5000
