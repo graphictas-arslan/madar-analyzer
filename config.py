11a826b61aa76cb450e3db9cbd994a60629c6a89
graphictas-arslan <graphictas@gmail.com>
2026-06-21 14:43:44 +0330

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,17 @@ load_dotenv()
 
 class Config:
 
-    SECRET_KEY = os.environ.get(
+    SECRET_KEY = os.getenv(
         "SECRET_KEY",
         "change-this-secret"
     )
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///bot.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-BALE_TOKEN = os.environ.get("BALE_TOKEN")
+BALE_TOKEN = os.getenv("BALE_TOKEN")
