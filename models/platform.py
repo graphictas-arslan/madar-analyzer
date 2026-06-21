b303b42aa1c6cd1b587f18de95ab93eca9e9d746
graphictas-arslan <graphictas@gmail.com>
2026-06-21 17:04:05 +0330

    Update platform.py

--- models/platform.py
+++ models/platform.py
@@ -1,1 +1,36 @@
+from extensions import db
+from datetime import datetime
 
+
+class Platform(db.Model):
+
+    __tablename__ = "platforms"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String(50),
+        unique=True,
+        nullable=False
+    )
+
+    title = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    is_active = db.Column(
+        db.Boolean,
+        default=True
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"<Platform {self.name}>"
