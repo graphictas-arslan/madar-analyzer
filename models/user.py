8ca072f794a867de1282cf717bd22dedab30aabe
graphictas-arslan <graphictas@gmail.com>
2026-06-21 17:05:30 +0330

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,57 @@
+from extensions import db
+from datetime import datetime
 
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    full_name = db.Column(
+        db.String(150),
+        nullable=False
+    )
+
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    mobile = db.Column(
+        db.String(20),
+        unique=True
+    )
+
+    password_hash = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    is_active = db.Column(
+        db.Boolean,
+        default=True
+    )
+
+    is_super_admin = db.Column(
+        db.Boolean,
+        default=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"<User {self.username}>"
