52af8b8f005f36bac9724b443e5039744eba2999
graphictas-arslan <graphictas@gmail.com>
2026-06-21 17:27:21 +0330

    Update permission.py

--- models/permission.py
+++ models/permission.py
@@ -1,1 +1,30 @@
+from extensions import db
+from datetime import datetime
 
+
+class Permission(db.Model):
+
+    __tablename__ = "permissions"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"<Permission {self.name}>"
