86b54df7588a54828c62d1b40a79557414eddce9
graphictas-arslan <graphictas@gmail.com>
2026-06-22 14:21:24 +0330

    Update routes.py

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -1,1 +1,15 @@
+from flask import Blueprint
+from flask import render_template
 
+dashboard_bp = Blueprint(
+    "dashboard",
+    __name__
+)
+
+
+@dashboard_bp.route("/channels/create")
+def create_channel():
+
+    return render_template(
+        "create_channel.html"
+    )
