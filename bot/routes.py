8bcd2c2e3013a303c44b26243e4fe819e9dd9a45
graphictas-arslan <graphictas@gmail.com>
2026-06-22 09:35:56 +0330

    Update routes.py

--- bot/routes.py
+++ bot/routes.py
@@ -1,1 +1,22 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
 
+bot_bp = Blueprint(
+    "bot",
+    __name__
+)
+
+
+@bot_bp.route("/webhook", methods=["POST"])
+def webhook():
+
+    update = request.get_json()
+
+    print(update)
+
+    return jsonify(
+        {
+            "status": "ok"
+        }
+    )
