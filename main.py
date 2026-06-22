ccc7ea9c9070653679bda798042ff14eadded640
graphictas-arslan <graphictas@gmail.com>
2026-06-22 21:22:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from config import Config
 from database import init_database
 from extensions import db, migrate
 from auth.routes import bot_bp
-import models
 from dashboard.routes import dashboard_bp
-
-app.register_blueprint(
-    dashboard_bp
-)
+import models
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
-app.register_blueprint(bot_bp)
+app.config["SECRET_KEY"] = "tas-super-secret-key"
 
 db.init_app(app)
 migrate.init_app(app, db)
+
+app.register_blueprint(bot_bp)
+app.register_blueprint(dashboard_bp)
 
 init_database(app)
 
@@ -32,5 +30,3 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=5000
     )
-
-app.config["SECRET_KEY"] = "tas-super-secret-key"
