87bee6b850afb638a7ac57334a345564df8cea6d
graphictas-arslan <graphictas@gmail.com>
2026-06-23 19:59:59 +0330

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,57 +1,22 @@
 from extensions import db
 from datetime import datetime
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
-
     __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    mobile = db.Column(db.String(20), unique=True)
+    role = db.Column(db.String(50), default="channel_admin")
+    is_active = db.Column(db.Boolean, default=True)
+    is_super_admin = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    full_name = db.Column(
-        db.String(150),
-        nullable=False
-    )
-
-    username = db.Column(
-        db.String(100),
-        unique=True,
-        nullable=False
-    )
-
-    mobile = db.Column(
-        db.String(20),
-        unique=True
-    )
-
-    password_hash = db.Column(
-        db.String(255),
-        nullable=False
-    )
-
-    is_active = db.Column(
-        db.Boolean,
-        default=True
-    )
-
-    is_super_admin = db.Column(
-        db.Boolean,
-        default=False
-    )
-
-    created_at = db.Column(
-        db.DateTime,
-        default=datetime.utcnow
-    )
-
-    updated_at = db.Column(
-        db.DateTime,
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow
-    )
-
-    def __repr__(self):
-        return f"<User {self.username}>"
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
