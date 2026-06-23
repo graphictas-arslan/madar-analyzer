ba471c5fe87c28ddb65bf7b1ec0683fb3f66db6c
graphictas-arslan <graphictas@gmail.com>
2026-06-23 20:57:24 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///madar.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:cDFAcUaUfElEhekNHEnlyFupaegkKqPe@postgres.railway.internal:5432/railway")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BALE_TOKEN = os.getenv("BALE_TOKEN")
