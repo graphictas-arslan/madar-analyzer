2717d7c83e0aee4b681874c4ba724955bd337cb2
graphictas-arslan <graphictas@gmail.com>
2026-06-25 22:38:53 +0330

    Update __init__.py

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,1 +1,13 @@
+from flask import Blueprint
 
+dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
+
+# ایمپورت همه مسیرها
+from . import organizations
+from . import channels
+from . import posts
+from . import bots
+from . import platforms
+from . import instagram
+from . import db_manage
+from . import assign
