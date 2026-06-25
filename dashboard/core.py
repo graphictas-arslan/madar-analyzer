220e4426ac7aa874697736e12bc66c2bdec3ad5f
graphictas-arslan <graphictas@gmail.com>
2026-06-26 00:09:53 +0330

    Delete dashboard directory

--- dashboard/core.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
--- dashboard/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, redirect, url_for, session
-from .core import dashboard_bp
-
-@dashboard_bp.route("/")
-def dashboard():
-    if "user_id" not in session:
-        return redirect(url_for("auth.login"))
-    return render_template("dashboard/index.html")
