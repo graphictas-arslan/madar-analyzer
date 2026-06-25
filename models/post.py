790eca765524ca5cb8a4e55aade4ec09d6b6d142
graphictas-arslan <graphictas@gmail.com>
2026-06-25 04:19:30 +0330

    Update post.py

--- models/post.py
+++ models/post.py
@@ -3,11 +3,12 @@ from datetime import datetime
 
 class Post(db.Model):
     __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
     channel_id = db.Column(db.Integer, db.ForeignKey("channels.id"), nullable=False, index=True)
-    bale_post_id = db.Column(db.String(200), nullable=False)
+    platform_post_id = db.Column(db.String(200), nullable=False)  # تغییر نام
     author_name = db.Column(db.String(200))
-    content_type = db.Column(db.String(50), nullable=False)
+    post_type = db.Column(db.String(50), nullable=False)
     text = db.Column(db.Text)
     caption = db.Column(db.Text)
     media_url = db.Column(db.Text)
@@ -18,7 +19,10 @@ class Post(db.Model):
     shares = db.Column(db.Integer, default=0)
     score = db.Column(db.Float, default=0)
     ai_score = db.Column(db.Float, default=0)
-    publish_time = db.Column(db.DateTime)
+    publish_date = db.Column(db.DateTime)
     status = db.Column(db.String(50), default="pending")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Post {self.platform_post_id}>"
