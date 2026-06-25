184aefa0baaae4defe016fb95e02adedc34a7b0a
graphictas-arslan <graphictas@gmail.com>
2026-06-25 23:57:06 +0330

    Update instagram.py

--- dashboard/instagram.py
+++ dashboard/instagram.py
@@ -1,9 +1,8 @@
 from flask import render_template, redirect, url_for, session, request, flash
 from extensions import db
-from models import InstagramPage, InstagramPost
-from datetime import datetime
-import requests
-from . import dashboard_bp
+from models import Organization, Channel, Post
+from sqlalchemy import func
+from .core import dashboard_bp  # این خط مهم است
 
 @dashboard_bp.route("/instagram")
 def instagram_pages():
