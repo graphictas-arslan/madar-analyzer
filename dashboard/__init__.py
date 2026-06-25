e596dd0bdcbcba11debd5d534a089b2f383141b4
graphictas-arslan <graphictas@gmail.com>
2026-06-25 22:57:59 +0330

    Update __init__.py

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
-# ایمپورت همه مسیرها
+# ایمپورت مسیرها بعد از ساخت blueprint (برای جلوگیری از circular import)
 from . import organizations
 from . import channels
 from . import posts
